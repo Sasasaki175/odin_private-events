9851f5c3cf211e69dd7e7925fa3c2ed1a4faeb5e
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2026-09-02 13:42:50 +0900

    feat: create index views for events and add a name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_01_050529) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_02_043548) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "location"
+    t.string "name"
     t.date "starts_on"
     t.datetime "updated_at", null: false
   end
