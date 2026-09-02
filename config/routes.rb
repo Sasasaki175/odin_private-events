9a4c35a10ad23d2d8e1f0f51da421633ec48fd56
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2026-09-02 14:02:17 +0900

    feat: set up devise and add User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root "events#index"
+  
   resources :events, only: [:index]
 end
