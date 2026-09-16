5d317f6e22dc9831c7bc910e46dc51d409b34a52
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2026-09-16 15:05:25 +0900

    fix: use correct route helper in users show test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
   test "should get show" do
-    get users_show_url
+    get user_url(@user)
     assert_response :success
   end
 end
